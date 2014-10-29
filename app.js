ee8bbe4500445dd94d83d87177784f42103b7f37
Martijn Hols <martijnhols@hotmail.com>
2014-10-29 10:43:41 +0100

    Change dir structure
    "apps" dir toegevoegd en "client" map hierin gezet onder de naam "student", deze is bereikbaar onder het pad /. "docent" dir toegevoegd voor docent app (nu nog leeg), deze is bereikbaar onder het pad /docent.

--- app.js
+++ app.js
@@ -1,91 +1,61 @@
-var express    = require('express');
-var path       = require('path');
-var http       = require('http');
-var socketio   = require('socket.io');
+var express = require('express');
+var path = require('path');
+var http = require('http');
+var socketio = require('socket.io');
 
 /* * * * * * * * * * * * * * * * * * * * * * */
 /*     Setup                                 */
 /* * * * * * * * * * * * * * * * * * * * * * */
 
-var app        = express();
+var app = express();
 var httpServer = http.Server(app);
-var io         = socketio(httpServer);
+var io = socketio(httpServer);
 
-app.use(express.static(path.join(__dirname, 'client')));
-
-/* * * * * * * * * * * * * * * * * * * * * * */
-/*     HTTP Routes                           */
-/* * * * * * * * * * * * * * * * * * * * * * */
-
-
-var mainRoutes = express.Router();
-  // get the list of movies
-
-mainRoutes.get('/', function(req, res) {
-   res.send("<h1>Hi there</h1>");
-});
-app.use(mainRoutes);
-
-
-/* * * * * * * * * * * * * * * * * * * * * * */
-/*     WebSocket Messages                    */
-/* * * * * * * * * * * * * * * * * * * * * * */
+app.use(express.static(path.join(__dirname, 'apps/main')));
+app.use('/', express.static(path.join(__dirname, 'apps/student')));
+app.use('/docent', express.static(path.join(__dirname, 'apps/docent')));
 
 var randomColor = require("./library/randomColor.js");
 
 var players = {};  // Using an object istead of an array is
-                   // useful because we mostly ned to find players by id
+// useful because we mostly ned to find players by id
 
+io.on("connection", function (socket) {
+	console.log("CONNECT:", socket.id);
 
-io.on("connection",function(socket){
-   console.log("CONNECT:", socket.id);
+	socket.on("disconnect", function () {
+		console.log("DISCONNECT", socket.id);
+		// Let's handle disconnects by informing everyone,
+		// and removing the player from our players.
+		socket.broadcast.emit("player has left", socket.id)  // the socket id is enough for
+		// all others to remove the player.
+		delete players[socket.id]  // this removes the player from the object.
+	});
 
-   socket.on("disconnect", function(){
-      console.log("DISCONNECT", socket.id);
-      // Let's handle disconnects by informing everyone,
-      // and removing the player from our players.
-      socket.broadcast.emit("player has left", socket.id)  // the socket id is enough for
-                                                           // all others to remove the player.
-      delete players[socket.id]  // this removes the player from the object.
-   });
+	socket.on("sign in", function (name) {
+		console.log("SIGN IN:", name);
 
-   socket.on("sign in", function(name){
-      console.log("SIGN IN:", name);
+		var newPlayer = {
+			name: name,
+			x: Math.floor(Math.random() * 300) + 50,
+			y: Math.floor(Math.random() * 300) + 50,
+			color: randomColor({ luminosity: 'bright'}),
+			socketId: socket.id     // This id is useful to identify players.
+			// We don't have to worry about duplicate player names.
+		};
+		players[newPlayer.socketId] = newPlayer;
+		socket.emit("sign in reply", players);
+		socket.broadcast.emit("new player", newPlayer);
+	})
 
-      var newPlayer = {
-         name:     name,
-         x:        Math.floor(Math.random()*300)+50,
-         y:        Math.floor(Math.random()*300)+50,
-         color:    randomColor({ luminosity: 'bright'}),
-         socketId: socket.id     // This id is useful to identify players.
-                                 // We don't have to worry about duplicate player names.
-      };
-      players[newPlayer.socketId] = newPlayer;
-      socket.emit("sign in reply", players );
-      socket.broadcast.emit("new player",newPlayer);
-   })
+	socket.on("update player", function (playerInfo) {
+		socket.broadcast.emit("update other player", playerInfo);
 
-   socket.on("update player", function(playerInfo){
-      socket.broadcast.emit("update other player", playerInfo);
-
-      // In class, we used a for-loop to find the player in the array, like this:
-         // for( var idx=0; idx< players.length; idx++) {
-         //    if( playerInfo.name === players[idx].name ) {
-         //       players[idx] = playerInfo;
-         //    }
-         // }
-      // Now, because we use an object to store all players, we just need this:
-      players[playerInfo.socketId] = playerInfo;
-   });
-
+		players[playerInfo.socketId] = playerInfo;
+	});
 
 });
 
-
-/* * * * * * * * * * * * * * * * * * * * * * */
-/*     Rock 'n Roll...                       */
-/* * * * * * * * * * * * * * * * * * * * * * */
-
-httpServer.listen(3000, function(){
-   console.log("Server running on port 3000.");
+httpServer.listen(3000, function () {
+	console.log("Server running on port 3000.");
 });
---
+++ apps/student/js/main.js
@@ -0,0 +1,111 @@
+var app = angular.module("angularSocketBlocks", ["ngRoute"]);
+
+// This factory is useful in other applications that use AngularJS with
+// Socket.IO 1.0 (or higher) and Express 4.
+// It is adapted from http://briantford.com/blog/angular-socket-io,
+// and an extended version can be found here: https://github.com/btford/angular-socket-io
+// (although I don't know if that version works with Express 4 and SocketIO 1.0)
+
+app.factory('socketIO', function ($rootScope) {
+	var socket = io();
+	socket.on("connect", function () {
+		console.log("connected", socket.io.engine.id);
+	});
+	return {
+		on: function (eventName, callback) {
+			socket.on(eventName, function () {
+				var args = arguments;
+				$rootScope.$apply(function () {
+					callback.apply(socket, args);
+				});
+			});
+		},
+		emit: function (eventName, data, callback) {
+			socket.emit(eventName, data, function () {
+				var args = arguments;
+				$rootScope.$apply(function () {
+					if (callback) {
+						callback.apply(socket, args);
+					}
+				});
+			})
+		},
+		id: function () {
+			return socket.io.engine.id
+		}
+	};
+});
+
+// end of Socket.IO service for AngularJS
+
+app.config(function ($routeProvider) {
+
+	$routeProvider.when('/name', {
+			templateUrl: '/templates/nameForm.html',
+			controller: 'nameFormController'
+		}).when('/blocks/:name', {
+			templateUrl: '/templates/blocks.html',
+			controller: 'blocksController'
+		}).otherwise({
+			redirectTo: '/name'
+		});
+});
+
+app.controller("nameFormController", function ($scope, $location) {
+
+	$scope.userName = "";
+
+	$scope.submitUserName = function () {
+		$location.path("/blocks/" + $scope.userName);
+	}
+
+});
+
+app.controller("blocksController", function ($scope, $routeParams, socketIO) {
+	socketIO.emit("sign in", $routeParams.name);
+
+	socketIO.on("sign in reply", function (players) {
+		console.log("SIGNINREPLY:", players);
+		$scope.playerSet = players;
+		$scope.player = $scope.playerSet[ socketIO.id() ]; // We're storing the player in an object
+		// indexed by socket-id (see server code).
+		// We no longer have to rely on the current
+		// player being the last player in the array.
+	});
+	socketIO.on("new player", function (newPlayerInfo) {
+		$scope.playerSet[newPlayerInfo.socketId] = newPlayerInfo;
+	});
+	socketIO.on("update other player", function (playerInfo) {
+		$scope.playerSet[playerInfo.socketId] = playerInfo;
+	});
+	socketIO.on("player has left", function (id) {
+		delete $scope.playerSet[id];
+	});
+
+	// This is no longer an array. We'll use a JS object where the keys
+	// are socket ids, and the values are player-objects.
+	// Therefore the name is changed from "playerList" to "playerSet".
+	$scope.playerSet = null;
+	$scope.player = null;
+
+	$scope.key = function (evt) {
+		console.log("KEY:", evt.keyCode);
+
+		var step = 15;
+
+		if (evt.keyCode == 37) { //LEFT
+			$scope.player.x -= step;
+			evt.preventDefault();
+		} else if (evt.keyCode == 38) { //UP
+			$scope.player.y -= step;
+			evt.preventDefault();
+		} else if (evt.keyCode == 39) { //RIGHT
+			$scope.player.x += step;
+			evt.preventDefault();
+		} else if (evt.keyCode == 40) { //DOWN
+			$scope.player.y += step;
+			evt.preventDefault();
+		}
+		socketIO.emit("update player", $scope.player);
+	}
+});
